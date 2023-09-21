8c0679e0e6f83e861baf0b4d9c05f48f65860121
Glauber Costa <glauber@turso.tech>
2023-09-21 13:27:09 -0400

    improve the example to show time deltas

--- remote-based-sync/index.ts
+++ remote-based-sync/index.ts
@@ -1,23 +1,47 @@
 import { createClient } from "@libsql/client";
 
-if (process.env.SYNC_URL === undefined) {
-  throw new Error("SYNC_URL must be set");
+if (process.env.DB_URL === undefined) {
+  throw new Error("DB_URL must be set");
 }
 
 if (process.env.AUTH_TOKEN === undefined) {
   throw new Error("AUTH_TOKEN must be set");
 }
 
+if (process.env.DB_URL != undefined && process.env.SYNC_URL != undefined) {
+  throw new Error("only one of DB_URL and SYNC_URL should be set");
+}
+
+function prettyPrintDuration(duration) {
+  if (duration >= 1000) {
+    return (duration / 1000).toFixed(0) + "ms";
+  } else {
+    return duration.toFixed(0) + "us";
+  }
+}
+
 const client = createClient({
-  url: "file:local.db",
-  syncUrl: process.env.SYNC_URL,
+  url: process.env.DB_URL,
   authToken: process.env.AUTH_TOKEN,
+  syncUrl: process.env.SYNC_URL ?? "",
 });
 
-await client.sync();
+if (process.env.SYNC_URL != undefined) {
+  await client.sync();
+}
 
-const rs = await client.execute("SELECT u.name, k.expired FROM users u JOIN keycards as k ON u.user_id = k.user_id");
+for (let i = 0; i < 10; i++) {
+  const start = Bun.nanoseconds();
+  const rs = await client.execute(
+    "SELECT u.name, k.expired FROM users u JOIN keycards as k ON u.user_id = k.user_id",
+  );
+  const delta = prettyPrintDuration((Bun.nanoseconds() - start) / 1000);
 
-for (const row of rs.rows) {
-  console.log(`The keycard for user ${row.name} is ${row.expired ? "expired" : "valid"}`);
+  for (const row of rs.rows) {
+    console.log(
+      `The keycard for user ${row.name} is ${
+        row.expired ? "expired" : "valid"
+      }. Query took ${delta}`,
+    );
+  }
 }
